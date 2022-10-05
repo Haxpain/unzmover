21a041a802f019a4830a69d233cf2e65b51e301c
Dave Meaker <100788068+Haxpain@users.noreply.github.com>
2022-10-05 13:06:50 +0100

    Initial Commit

---
+++ unzmover/unzmoverApp.swift
@@ -0,0 +1,17 @@
+//
+//  unzmoverApp.swift
+//  unzmover
+//
+//  Created by Dave Meaker on 05/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct unzmoverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
